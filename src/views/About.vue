ec74b17fb890e3ecde99e3f6f874a30275e23117
Damien LAURENS <damienlaurens64@gmail.com>
2019-03-12 20:49:17 +0100

    Initialisation de l'environnement : initialisation du router + axios + création d'un fichier scss global

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,40 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="maClass">This is an about page with my text : {{message}}</h1>
+      <button v-on:click="affiche">TEST méthode</button>
+      <div v-for="page in pages">
+          {{page.content.rendered}}
+      </div>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import axios from 'axios';
+
+@Component
+export default class About extends Vue {
+  private message: string = 'Test';
+  private urlPages: string = 'http://localhost/wp-json/wp/v2/pages';
+  private pages = [];
+
+  public affiche() {
+    alert('TEST2');
+  }
+
+  public created() {
+    axios.get(this.urlPages)
+      .then((response) => {
+        this.pages = response.data;
+      });
+  }
+
+}
+</script>
+
+<style lang="scss">
+
+  .maClass{
+    color: $color;
+  }
+</style>
