4b1c2dcff6f4fb57f5bcea9fff0bb8a4133a283c
Damien LAURENS <damienlaurens64@gmail.com>
2019-08-04 23:13:11 +0200

    Initialisation du texte de la page d'accueil

--- src/ts/constants.ts
+++ src/ts/constants.ts
@@ -1,2 +1,3 @@
 // URLS REST
 export const URL_PAGES = process.env.VUE_APP_API_PATH + '/pages';
+export const URL_PAGE_ACCUEIL = process.env.VUE_APP_API_PATH + '/pages/6';
--- src/views/Accueil.vue
+++ src/views/Accueil.vue
@@ -6,6 +6,9 @@
                 <Separator/>
                 Page d'accueil<br>
                 Page d'accueil<br>
+                <div v-for="page in accueilPage">
+                    {{page.content.rendered}}
+                </div>
             </div>
         </div>
     </div>
@@ -14,11 +17,25 @@
 <script lang="ts">
     import { Component, Vue } from 'vue-property-decorator';
     import Separator from '@/components/Separator.vue';
+    import axios from 'axios';
+    import * as Constants from '@/ts/constants';
 
     @Component({
       components: {
         Separator,
       },
     })
-    export default class Accueil extends Vue {}
+    export default class Accueil extends Vue {
+      private accueilPage = [];
+
+      public created() {
+        axios.get(Constants.URL_PAGE_ACCUEIL)
+          .then((response) => {
+            this.accueilPage = response.data;
+          })
+          .catch((error) => {
+            window.location.href = '/error';
+          });
+      }
+    }
 </script>
