d5757326e76ddf5e89a577e1d5c74a9bfdcf5585
Damien LAURENS <damienlaurens64@gmail.com>
2019-10-13 19:38:52 +0200

    Initialisation de la fiche produit

---
+++ src/views/ProductSheet.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="row">
+        <div class="col-12">
+            <Product v-bind:fullDescription="true" v-bind:productPath="$route.params.productPath"/>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+  import {Component, Prop, Vue} from 'vue-property-decorator';
+  import Product from '@/components/Product.vue';
+
+  @Component({
+    components: {
+      Product,
+    },
+  })
+  export default class ProductSheet extends Vue {
+    @Prop() private productPath!: string;
+  }
+</script>
