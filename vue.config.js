8af8ca60905d22c88e0bb27af8e0e9e0603844c1
Damien LAURENS <damienlaurens64@gmail.com>
2019-10-31 14:34:35 +0100

    Construction du livrable de PROD

--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,20 @@ module.exports = {
                 data: `@import "@/scss/global.scss";`
             }
         }
+    },
+    chainWebpack: config => {
+        if(config.plugins.has('extract-css')) {
+            const extractCSSPlugin = config.plugin('extract-css')
+            extractCSSPlugin && extractCSSPlugin.tap(() => [{
+                filename: '[name].css',
+                chunkFilename: '[name].css'
+            }])
+        }
+    },
+    configureWebpack: {
+        output: {
+            filename: '[name].js',
+            chunkFilename: '[name].js'
+        }
     }
 };
