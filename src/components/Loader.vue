8698a6dd8c5eed1b87672629e8a281b6af3f9827
Damien LAURENS <damienlaurens64@gmail.com>
2020-03-08 16:17:08 +0100

    Création du composant Loader + utilisation dans la HomePage

---
+++ src/components/Loader.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="header">
+        <div v-if="isLoading">
+            <Spinner/>
+        </div>
+        <div v-else>
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+  import {Component, Prop, Vue} from 'vue-property-decorator';
+  import {ViewState} from '@/ts/Models';
+  import Spinner from '@/components/Spinner.vue';
+  @Component({
+    components: {
+      Spinner,
+    },
+  })
+    export default class Loader extends Vue {
+        @Prop() private loading!: ViewState;
+
+        get isLoading(): boolean {
+            return this.loading === ViewState.Loading;
+        }
+    }
+</script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,37 +1,36 @@
 <template>
     <div class="home-page container-fluid limit-max-width">
-        <div v-if="isLoading">
-            <Spinner/>
-        </div>
-        <div v-else>
-            <div class="row mt-lg-5">
-                <div class="col-12 col-lg-6 pt-3" v-html="homePage.image"></div>
+        <Loader v-bind:loading="viewState">
+            <div v-if="homePageDatas != null" class="row mt-lg-5">
+                <div class="col-12 col-lg-6 pt-3" v-html="homePageDatas.image"></div>
                 <div class="col-12 col-lg-6">
-                    <h1 class="text-center" v-html="homePage.title"></h1>
+                    <h1 class="text-center" v-html="homePageDatas.title"></h1>
                     <Separator/>
-                    <div class="text-center" v-html="homePage.content"></div>
+                    <div class="text-center" v-html="homePageDatas.content"></div>
                 </div>
                 <Production/>
             </div>
             <div class="row">
                 <div class="col-12 px-0">
-                    <Coordinates class="col-12 py-3 pb-lg-5 text-center "/>
+                    <Coordinates class="col-12 py-3 pb-lg-5 text-center"/>
                 </div>
             </div>
-        </div>
+        </Loader>
     </div>
 </template>
 
 <script lang="ts">
-  import {Component, Vue, Watch} from 'vue-property-decorator';
+  import {Component, Vue} from 'vue-property-decorator';
   import Separator from '@/components/Separator.vue';
   import Production from '@/components/Production.vue';
   import Coordinates from '@/components/Coordinates.vue';
   import Spinner from '@/components/Spinner.vue';
-  import {HomePageDatas, ViewState, WpDatas} from '@/ts/Models';
+  import {HomePageDatas, ViewState} from '@/ts/Models';
+  import Loader from '@/components/Loader.vue';
 
   @Component({
     components: {
+      Loader,
       Coordinates,
       Separator,
       Production,
@@ -40,38 +39,16 @@
   })
   export default class HomePage extends Vue {
     private viewState: ViewState = ViewState.Loading;
-    private homePage?: HomePageDatas;
+
+    get homePageDatas(): HomePageDatas {
+      if (this.$store.getters.wpDatas.homePageDatas !== undefined) {
+        this.viewState = ViewState.Loaded;
+      }
+      return this.$store.getters.wpDatas.homePageDatas;
+    }
 
     public created() {
-      if (this.wpDatas?.homePageDatas) {
-        this.viewState = ViewState.Loaded;
-        this.homePage = this.wpDatas.homePageDatas;
-      } else {
-        this.viewState = ViewState.Loading;
-        this.$store.dispatch('loadWpDatas');
-      }
-    }
-
-    @Watch('wpDatas')
-    public onWpDatasChange(wpDatas: WpDatas) {
-      if (this.wpDatas?.homePageDatas) {
-        this.viewState = ViewState.Loaded;
-        this.homePage = wpDatas.homePageDatas;
-      } else {
-        this.viewState = ViewState.Error;
-      }
-    }
-
-    get wpDatas(): WpDatas {
-      return this.$store.getters.wpDatas;
-    }
-
-    get isLoading(): boolean {
-      return this.viewState === ViewState.Loading;
-    }
-
-    get isError(): boolean {
-      return this.viewState === ViewState.Error;
+      this.$store.dispatch('loadWpDatas');
     }
   }
 </script>
