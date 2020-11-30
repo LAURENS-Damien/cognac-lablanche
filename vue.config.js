99ef23c1a447d64290839677d9a1aa01e6e80666
Damien Laurens <damien.laurens.ext@orange.com>
2020-11-30 23:09:08 +0100

    Mise à jour des dépendances

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
             sass: {
                 // @/ is an alias to src/
                 // so this assumes you have a file named `src/variables.scss`
-                data: `@import "@/scss/global.scss";`
+                additionalData: `@import "@/scss/global.scss";`
             }
         }
     },
