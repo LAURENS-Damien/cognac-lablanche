1683bfd1d43f191d37fa33c7750e40c2476948ff
Damien LAURENS <damienlaurens64@gmail.com>
2022-03-31 22:26:21 +0200

    On passe par une balise <picture> pour afficher des images

--- wordpress/wp-content/themes/cognaclablanche/functions.php
+++ wordpress/wp-content/themes/cognaclablanche/functions.php
@@ -1,4 +1,10 @@
 <?php
+add_image_size( 'xs', 576 );
+add_image_size( 'sm', 768 );
+add_image_size( 'md', 992 );
+add_image_size( 'lg', 1200 );
+add_image_size( 'xl', 1400 );
+
 function theme_js()
 {
     wp_enqueue_style('app.css',
@@ -262,7 +268,9 @@ function get_product($data): ?array
 
     $product["id"] = $page->ID;
     $product["title"] = $page->post_title;
-    $product["image"] = get_image($page->post_content);
+//     $product["image"] = get_image($page->post_content);
+    $product["image"] = create_responsive_image($page->post_content, $page);
+
     // Les ACF sont récupérés directement dans le composant, à voir si on peut retourner proprement ici
 
     if (empty($page)) {
@@ -426,3 +434,77 @@ function add_lazy_loading($str,$insertstr,$pos)
 
     return $new_str;
 }
+
+function test_create_responsive_image($data): ?array
+{
+    $page = get_page_by_path('catalogue/' . $data['category'] . '/' . $data['product']);
+
+    $product["id"] = $page->ID;
+    $product["title"] = $page->post_title;
+    $product["image"] = create_responsive_image($page->post_content, $page);
+    // Les ACF sont récupérés directement dans le composant, à voir si on peut retourner proprement ici
+
+    if (empty($page)) {
+        return null;
+    }
+
+    return $product;
+}
+
+function get_image_id($content)
+{
+    $start = strpos($content, "<!-- wp:image {\"id\":") + 20;
+    $end = strpos($content, ",");
+
+    return substr($content, $start, $end - $start);
+}
+
+function create_responsive_image($image, $page) {
+    $image_src = get_image_src($image);
+    $image_id = get_image_id($page->post_content);
+    $img_xs = wp_get_attachment_image_src($image_id, 'xs')[0];
+    $img_sm = wp_get_attachment_image_src($image_id, 'sm')[0];
+    $img_md = wp_get_attachment_image_src($image_id, 'md')[0];
+    $img_lg = wp_get_attachment_image_src($image_id, 'lg')[0];
+    $img_xl = wp_get_attachment_image_src($image_id, 'xl')[0];
+
+    return '<picture class="wp-block-image size-full">
+<source media="(max-width: 575px)" srcset="'. $img_xs .'"/>
+<source media="(min-width: 576px) and (max-width: 767px)" srcset="'. $img_sm .'"/>
+<source media="(min-width: 768px) and (max-width: 991px)" srcset="'. $img_md .'"/>
+<source media="(min-width: 992px) and (max-width: 1199px)" srcset="'. $img_lg .'"/>
+<source media="(min-width: 1200px)" srcset="'. $img_xl .'"/>
+<img loading="lazy" alt="" src="'. $image_src .'"/>
+</picture>';
+}
+
+function get_image_src($image) {
+    $regex_result = array();
+    preg_match( '/<img src="([^"]*)"/i', $image, $regex_result);
+
+    return $regex_result[1];
+}
+
+add_action('rest_api_init', function () {
+    register_rest_route('wp/v2', '/test/(?P<category>\S+)/(?P<product>\S+)', array(
+        'methods' => 'GET',
+        'callback' => 'test_create_responsive_image',
+    ));
+});
+
+// function get_image_url($image_src, $image_name) {
+//     $image_url = str_replace($image_name, "", $image_src);
+//     return $image_url;
+// }
+//
+// function get_image_name($image_src) {
+//     $regex_result = array();
+//     preg_match( '/([\w\d_-]*)\.?[^\\\\\/]*$/i', $image_src, $regex_result);
+//     return $regex_result[0];
+// }
+//
+// function get_image_extension($image_src) {
+//     $regex_result = array();
+//     preg_match( '/([\w\d_-]*)\.?$/i', $image_src, $regex_result);
+//     return $regex_result[0];
+// }
